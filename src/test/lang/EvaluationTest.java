20e3e2039673f68c53a17793c23079ea0c7fca0b
Alexandru Dura <alexandru.dura@cs.lth.se>
2019-03-07 14:22:16 +0100

    Add an evaluation test for the typecheck DatalogM program.

--- src/test/lang/EvaluationTest.java
+++ src/test/lang/EvaluationTest.java
@@ -1,63 +1,94 @@
 package lang;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import lang.ast.Program;
 import lang.ast.FormalPredicate;
 import lang.ast.GlobalNames;
 import lang.ast.PredicateRef;
+import lang.ast.Program;
 import lang.ast.config.Description;
 import lang.io.CSVUtil;
 import lang.io.FileUtil;
 import lang.io.SimpleLogger;
-import lang.relation.Relation;
 import lang.relation.PseudoTuple;
+import lang.relation.Relation;
 
 public class EvaluationTest {
 	/*
 	 * Compare one evaluation scheme against another.
 	 */
+	public Map<String, Relation> doSingleEvaluation(Description d1) throws Exception {
+		Program program1 = d1.getTask().perform();
+		FormalPredicate fpOut1 = program1.formalPredicateMap().get(GlobalNames.OUTPUT_NAME);
+
+		if (fpOut1 == null)
+			assertTrue(false);
+
+		HashMap<String, Relation> nameToRel = new HashMap<>();
+		for (PseudoTuple ps : fpOut1.relation.tuples()) {
+			PredicateRef pr = (PredicateRef) ps.coord(0);
+			FormalPredicate fp = pr.formalpredicate();
+
+			File in1 = new File(d1.outputDir() + "/" + fp.predicateName() + ".csv");
+
+			Relation r1 = CSVUtil.readRelationFrom(program1, in1, fp.realArity());
+
+			SimpleLogger.logger().log("Read relation from: " + in1.getPath(), SimpleLogger.LogLevel.Level.DEBUG);
+			if (r1 == null) {
+				SimpleLogger.logger().log("Relation is null: " + in1.getPath(), SimpleLogger.LogLevel.Level.ERROR);
+				assertTrue(false);
+			}
+			nameToRel.put(fp.predicateName(), r1);
+
+		}
+		return nameToRel;
+	}
+
 	public void doEvaluationTest(Description d1, Description d2) throws Exception {
 		Program program1 = d1.getTask().perform();
 		Program program2 = d2.getTask().perform();
 
-        FormalPredicate fpOut1 = program1.formalPredicateMap().get(GlobalNames.OUTPUT_NAME);
-        FormalPredicate fpOut2 = program1.formalPredicateMap().get(GlobalNames.OUTPUT_NAME);
-        if(fpOut1 == null || fpOut2 == null) assertTrue(false);
+		FormalPredicate fpOut1 = program1.formalPredicateMap().get(GlobalNames.OUTPUT_NAME);
+		FormalPredicate fpOut2 = program1.formalPredicateMap().get(GlobalNames.OUTPUT_NAME);
+		if (fpOut1 == null || fpOut2 == null)
+			assertTrue(false);
 
-        assertTrue(fpOut1.relation.equals(fpOut2.relation));
+		assertTrue(fpOut1.relation.equals(fpOut2.relation));
 
-        for(PseudoTuple ps : fpOut1.relation.tuples()) {
-            PredicateRef pr = (PredicateRef) ps.coord(0);
-            FormalPredicate fp = pr.formalpredicate();
+		for (PseudoTuple ps : fpOut1.relation.tuples()) {
+			PredicateRef pr = (PredicateRef) ps.coord(0);
+			FormalPredicate fp = pr.formalpredicate();
 
-            File in1 = new File(d1.outputDir() + "/" + fp.predicateName() + ".csv");
-            Relation r1 = CSVUtil.readRelationFrom(program1, in1, fp.realArity());
-            
-            SimpleLogger.logger().log("Read r1 from: " + in1.getPath(), SimpleLogger.LogLevel.Level.DEBUG);
-            if (r1 == null) {
-                SimpleLogger.logger().log("r1 is null: " + in1.getPath(), SimpleLogger.LogLevel.Level.ERROR);
-                assertTrue(false);
-            }
+			File in1 = new File(d1.outputDir() + "/" + fp.predicateName() + ".csv");
+			Relation r1 = CSVUtil.readRelationFrom(program1, in1, fp.realArity());
 
-            File in2 = new File(d2.outputDir() + "/" + fp.predicateName() + ".csv");
-            SimpleLogger.logger().log("Read r2 from: " + in2.getPath(), SimpleLogger.LogLevel.Level.DEBUG);
-            Relation r2 = CSVUtil.readRelationFrom(program2, in2, fp.realArity());
-            if (r2 == null) {
-                SimpleLogger.logger().log("r2 is null: " + in2.getPath(), SimpleLogger.LogLevel.Level.ERROR);
-                assertTrue(false);
-            }
+			SimpleLogger.logger().log("Read r1 from: " + in1.getPath(), SimpleLogger.LogLevel.Level.DEBUG);
+			if (r1 == null) {
+				SimpleLogger.logger().log("r1 is null: " + in1.getPath(), SimpleLogger.LogLevel.Level.ERROR);
+				assertTrue(false);
+			}
 
-            SimpleLogger.logger().log("r1: " + r1.tuples(), SimpleLogger.LogLevel.Level.DEBUG);
-            SimpleLogger.logger().log("r2: " + r2.tuples(), SimpleLogger.LogLevel.Level.DEBUG);
-            assertTrue(r1.equals(r2));
-        }
+			File in2 = new File(d2.outputDir() + "/" + fp.predicateName() + ".csv");
+			SimpleLogger.logger().log("Read r2 from: " + in2.getPath(), SimpleLogger.LogLevel.Level.DEBUG);
+			Relation r2 = CSVUtil.readRelationFrom(program2, in2, fp.realArity());
+			if (r2 == null) {
+				SimpleLogger.logger().log("r2 is null: " + in2.getPath(), SimpleLogger.LogLevel.Level.ERROR);
+				assertTrue(false);
+			}
+
+			SimpleLogger.logger().log("r1: " + r1.tuples(), SimpleLogger.LogLevel.Level.DEBUG);
+			SimpleLogger.logger().log("r2: " + r2.tuples(), SimpleLogger.LogLevel.Level.DEBUG);
+			assertTrue(r1.equals(r2));
+		}
 	}
 
 	@DisplayName("Compare Internal Evaluation to Souffle")
@@ -66,7 +97,7 @@ public class EvaluationTest {
 			"evalTest_6.in", "evalTest_7.in", "evalTest_8.in", "evalTest_9.in" })
 	void evaluationTestsBottomUpNaiveCompareSouffle(String fileName) throws Exception {
 		String inputFile = "./tests/evaluation/" + fileName;
-		
+
 		SimpleLogger.logger().log("Input: " + inputFile, SimpleLogger.LogLevel.Level.DEBUG);
 		Description d1 = FileUtil.parseDescription(
 				"eval::souffle      -OUT ./tests/output/souffle -FACTS ./facts " + inputFile);
@@ -88,7 +119,7 @@ public class EvaluationTest {
 						+ fileName);
 		doEvaluationTest(d1, d2);
 	}
-	
+
 	@DisplayName("Compare Internal Evaluation to Souffle WithNEG")
 	@ParameterizedTest(name = "Evaluation Tests Valid WithNEG")
 	@ValueSource(strings = { "evalTest_1.in", "evalTest_2.in" })
@@ -102,7 +133,7 @@ public class EvaluationTest {
 						+ fileName);
 		doEvaluationTest(d1, d2);
 	}
-	
+
 	@DisplayName("Compare Internal Evaluation to Souffle WithBinPred")
 	@ParameterizedTest(name = "Evaluation Tests Valid")
 	@ValueSource(strings = { "evalTest_1.in", "evalTest_2.in","evalTest_3.in","evalTest_4.in","evalTest_5.in","evalTest_6.in", "evalTest_7.in","evalTest_8.in","evalTest_9.in" })
@@ -124,5 +155,16 @@ public class EvaluationTest {
                 "eval::bottomupnaive -OUT ./tests/output        -FACTS ./tests/evaluation/withmeta/facts ./tests/evaluation/withmeta/" + fileName);
         doEvaluationTest(d1, d2);
     }
-	
+
+    @DisplayName("Check the results using Souffle evaluation")
+	@ParameterizedTest(name = "IMPORT Tests Valid")
+    @ValueSource(strings = { "evalTest_1.in" })
+	void evaluationTestSouffleAndImport(String fileName) throws Exception {
+		Description d1 = FileUtil.parseDescription(
+				"eval::souffle -OUT ./tests/output/souffle -FACTS ./tests/evaluation/withimport/facts ./tests/evaluation/withimport/"
+						+ fileName);
+		Map<String, Relation> outRelations = doSingleEvaluation(d1);
+		assertEquals(outRelations.get("NotFullyTypedName").size(), 0);
+		assertEquals(outRelations.get("InconsistentTypedName").size(), 0);
+    }
 }

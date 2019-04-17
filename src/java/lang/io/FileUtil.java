68ea508bfca673d0f1f6bb727da5278c9fe83efe
Alexandru Dura <alexandru.dura@cs.lth.se>
2019-04-17 11:18:10 +0200

    Change the IMPORT statement to use the grammar in metalang

--- src/java/lang/io/FileUtil.java
+++ src/java/lang/io/FileUtil.java
@@ -30,30 +30,30 @@ public class FileUtil {
         if(index == -1) return fn;
         return fn.substring(0, index);
     }
-	
+
 	public static String fileName(String path) {
 		Path p = Paths.get(path);
 		return p.getFileName().toString();
 	}
-	
+
 	public static String normalize(String dir) {
 		if(dir.length() == 0) return dir;
 		if(dir.charAt(dir.length() - 1) == '/') return dir.substring(0, dir.length() - 1);
 		return dir;
 	}
-	
+
 	public static Program parse(String program) throws IOException, beaver.Parser.Exception {
 		LangScanner scanner = new LangScanner(new StringReader(program));
 		LangParser parser = new LangParser();
 		return (Program)parser.parse(scanner);
 	}
-	
+
 	public static Program parse(File file) throws IOException, beaver.Parser.Exception {
 		LangScanner scanner = new LangScanner(new FileReader(file));
 		LangParser parser = new LangParser();
 		return (Program)parser.parse(scanner);
 	}
-	
+
 	public static Description parseDescription(String s) throws IOException, beaver.Parser.Exception {
 		ConfigScanner configScanner = new ConfigScanner(new StringReader(s));
 		ConfigParser configParser   = new ConfigParser();

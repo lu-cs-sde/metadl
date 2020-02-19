144f77d37155d2805b56fb178638f1cb6e1c4fe2
Alexandru Dura <alexandru.dura@cs.lth.se>
2020-02-19 15:05:56 +0100

    Issue #6: Clean the semantics around opt and list
    The semantics of patterns now are:
    empty -> empty list
    ... (gap) -> any list, including an empty one
    `mv -> one element
    `mv, ... -> one or more elements
    
    The semantics of opt is:
    empty -> missing optional
    `mv -> some optional

--- src/test/lang/EvaluationTest.java
+++ src/test/lang/EvaluationTest.java
@@ -165,8 +165,7 @@ public class EvaluationTest {
 							 "evalTest_5", "evalTest_6", "evalTest_7",
 							 "evalTest_8", "evalTest_9", "evalTest_10",
 							 "evalTest_11", "evalTest_12", "evalTest_13",
-							 "evalTest_14",
-							 "evalTest_15"})
+							 "evalTest_14", "evalTest_15", "evalTest_16"})
 	void evaluationTestSoufflePatterns(String fileName) throws Exception {
 		Description d1 = FileUtil.parseDescription(
 		   "eval::souffle -OUT ./tests/output/souffle -FACTS ./tests/evaluation/withimport/facts ./tests/evaluation/withimport/"
@@ -183,7 +182,8 @@ public class EvaluationTest {
 				System.out.println(rel.getValue());
 
 			}
-			assertEquals(expectedRel, rel.getValue());
+			String msg = "Mismatch in test " + fileName + ", relation " + rel.getKey();
+			assertEquals(expectedRel, rel.getValue(), msg);
 		}
 	}
 }
---
+++ tests/evaluation/withimport/evalTest_16_input.java
@@ -0,0 +1,24 @@
+public abstract class A_1 extends B_1 implements C_1 { }
+public abstract class A_2 extends B_2 implements C_2, D_2 { }
+public abstract class A_3 extends B_3  { }
+public abstract class A_4  implements C_4 { }
+public abstract class A_5  implements C_5, D_5 { }
+public abstract class A_6   { }
+public  class A_7 extends B_7 implements C_7 { }
+public  class A_8 extends B_8 implements C_8, D_8 { }
+public  class A_9 extends B_9  { }
+public  class A_10  implements C_10 { }
+public  class A_11  implements C_11, D_11 { }
+public  class A_12   { }
+abstract class A_13 extends B_13 implements C_13 { }
+abstract class A_14 extends B_14 implements C_14, D_14 { }
+abstract class A_15 extends B_15  { }
+abstract class A_16  implements C_16 { }
+abstract class A_17  implements C_17, D_17 { }
+abstract class A_18   { }
+class A_19 extends B_19 implements C_19 { }
+class A_20 extends B_20 implements C_20, D_20 { }
+class A_21 extends B_21  { }
+class A_22  implements C_22 { }
+class A_23  implements C_23, D_23 { }
+class A_24   { }

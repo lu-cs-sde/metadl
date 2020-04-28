489daea5e6bbb2639b335b45044451c2b50b3e04
Alexandru Dura <alexandru.dura@cs.lth.se>
2020-04-28 12:09:24 +0200

    Handle this-qualified accesses in the unwritten field analysis

---
+++ tests/evaluation/metadl-java/src/unwritten-field/Test.java
@@ -0,0 +1,31 @@
+class C {
+	@NoWarning("UWF_UNWRITTEN_FIELD")
+	private String NOBUG1;
+
+	@NoWarning("UWF_UNWRITTEN_FIELD")
+	private String NOBUG2;
+
+	@ExpectWarning("UWF_UNWRITTEN_FIELD")
+	private String BUG;
+
+	public void WriteNoBug1() {
+		NOBUG1 = "Text";
+	}
+
+	public String ReadNoBug1() {
+		return this.NOBUG1;
+	}
+
+	public void WriteNoBug2() {
+		this.NOBUG2 = "Text";
+	}
+
+	public String ReadNoBug2() {
+		return NOBUG2;
+	}
+
+	public String Bug() {
+		return this.BUG;
+	}
+
+}

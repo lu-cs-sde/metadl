07cdf50e148422e73a12d43439bfd61d71fd3a4b
Alexandru Dura <alexandru.dura@cs.lth.se>
2021-03-18 11:43:38 +0100

    Traverse library code in Datalog projection

--- src/java/lang/java/obj/DatalogProjection2.java
+++ src/java/lang/java/obj/DatalogProjection2.java
@@ -150,7 +150,7 @@ public class DatalogProjection2 {
 														 DatalogProjectionSink tupleSink,
 														 String ...attrs) {
 
-		boolean mapAttributes = !isLibraryNode(currentCU) || deepAnalysis;
+		boolean skipNonPublicNodes = isLibraryNode(currentCU) && !deepAnalysis;
 		boolean fromSource = currentCU.fromSource();
 
 		Queue<ASTNode<?>> currentCUNodes = new ArrayDeque<>();
@@ -172,15 +172,18 @@ public class DatalogProjection2 {
 			}
 			// mark visited
 			processed.add(q);
+
+			if (skipNonPublicNodes && q.isPrivateOrPackage()) {
+				continue;
+			}
+
 			// add the tuples to the relation
 			toTuples(q, fileId, tupleSink.getAST(), fromSource);
 			// record source location
 			srcLoc(q, fileId, tupleSink.getSrcLoc());
 			// now process the attributes
-			if (mapAttributes) {
-				mapAttributes(q, currentCU, fileId, otherCompilationUnits, currentCUNodes, tupleSink.getAttributes(),
-							  tupleSink.getProvenance(), attrs);
-			}
+			mapAttributes(q, currentCU, fileId, otherCompilationUnits, currentCUNodes, tupleSink.getAttributes(),
+						  tupleSink.getProvenance(), attrs);
 			// add the children to the worklist
 			for (Pair<Integer, ASTNode> indexedChild : ASTNodeEnumerator.childrenOf(q, !fromSource)) {
 				if (indexedChild.getRight() != null)

31fab36bcccfc2df310b3d58a6429bd32019f07b
Alexandru Dura <alexandru.dura@cs.lth.se>
2021-06-24 21:04:58 +0200

    Package JavaDL and the evaluation framework in a Docker image

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+JAVADL_TOKEN=Td8iVr7GdD3Jv688hVBm
+JAVADL_INC_EVAL_TOKEN=4iDr7qWs1JHncUDDyRZy
+JAVADL_EVAL_TOKEN=jQ7vu8WkKzd3_EoAyDhL
+
+docker build \
+       --build-arg SOUFFLE_GIT=https://github.com/alexdura/souffle \
+       --build-arg SOUFFLE_HASH=oopsla21 \
+       --build-arg JAVADL_GIT=https://oauth2:$JAVADL_TOKEN@git.cs.lth.se/al7330du/metadl \
+       --build-arg JAVADL_HASH=bcc61d51230536659383e8ff4b485b96894c935c \
+       --build-arg JAVADL_INC_EVAL_GIT=https://oauth2:$JAVADL_INC_EVAL_TOKEN@git.cs.lth.se/al7330du/metadl-inc-eval \
+       --build-arg JAVADL_INC_EVAL_HASH=oopsla21 \
+       --build-arg JAVADL_EVAL_GIT=https://oauth2:$JAVADL_EVAL_TOKEN@git.cs.lth.se/al7330du/metadl-eval \
+       --build-arg JAVADL_EVAL_HASH=oospla21 \
+       -t javadl:oopsla21 .

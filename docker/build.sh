cf2e418ba8f554c1d3f68804bc79233306fbe757
Alexandru Dura <alexandru.dura@cs.lth.se>
2021-07-09 21:05:05 +0200

    Update the git hash for Souffle

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ JAVADL_EVAL_TOKEN=jQ7vu8WkKzd3_EoAyDhL
 
 docker build \
        --build-arg SOUFFLE_GIT=https://github.com/alexdura/souffle \
-       --build-arg SOUFFLE_HASH=oopsla21 \
+       --build-arg SOUFFLE_HASH=84826bb8b6ef739a762c992c06a42441d19b72da \
        --build-arg JAVADL_GIT=https://oauth2:$JAVADL_TOKEN@git.cs.lth.se/al7330du/metadl \
        --build-arg JAVADL_HASH=cf5a8ffde175b0d6a8bcaf4a5e9b199324f22859 \
        --build-arg JAVADL_INC_EVAL_GIT=https://oauth2:$JAVADL_INC_EVAL_TOKEN@git.cs.lth.se/al7330du/metadl-inc-eval \

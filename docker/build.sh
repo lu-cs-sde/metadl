789896b489832097631f898d8e4e589cb5836509
Christoph Reichenbach <creichen@gmail.com>
2021-08-10 06:38:00 +0000

    Docker build script hash updated

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,7 @@ docker build \
        --build-arg SOUFFLE_GIT=https://github.com/creichen/souffle \
        --build-arg SOUFFLE_HASH=no-signal \
        --build-arg JAVADL_GIT=https://oauth2:$JAVADL_TOKEN@git.cs.lth.se/al7330du/metadl \
-       --build-arg JAVADL_HASH=d2c973ae6c9b5429c68e23a0402058050871f473 \
+       --build-arg JAVADL_HASH=771780c58acff2ef6f2063eedc9aaa94bf596bb1 \
        --build-arg JAVADL_INC_EVAL_GIT=https://oauth2:$JAVADL_INC_EVAL_TOKEN@git.cs.lth.se/al7330du/metadl-inc-eval \
        --build-arg JAVADL_INC_EVAL_HASH=65b7e74ccfe3c995755703b117f274ad8cc5ce80 \
        --build-arg JAVADL_EVAL_GIT=https://oauth2:$JAVADL_EVAL_TOKEN@git.cs.lth.se/al7330du/metadl-eval \

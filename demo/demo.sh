8f7550ad7aab15be2f7dc3dd2baad15c2f8bab40
Alexandru Dura <alexandru.dura@cs.lth.se>
2022-11-23 23:19:35 +0100

    Demo: explain the loop depth

--- demo/demo.sh
+++ demo/demo.sh
@@ -6,4 +6,4 @@ java -jar $CLOG_JAR -e metadl \
      --lang c \
      --sources src/loops.c,A:src/duff.c,A:src/loops.cpp,A \
      --profile profile.json \
-     ../tests/clang/evaluation/loop_depth.mdl
+     loop_depth.mdl

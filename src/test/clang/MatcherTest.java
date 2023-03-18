9be6d08a65db9baf281bcbc9d419ab41c6b9b248
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-03-18 06:26:41 +0100

    Change indentation from tabs to spaces

--- src/test/clang/MatcherTest.java
+++ src/test/clang/MatcherTest.java
@@ -18,57 +18,57 @@ import se.lth.sep.ParseTree;
 
 
 public class MatcherTest {
-	private static List<lang.c.pat.ast.ASTNode> parse(String c, Category start) {
-		// System.out.println("================================================================================");
-		// System.out.println("Parsing: \"" + c + "\"");
-		// System.out.println("================================================================================");
+  private static List<lang.c.pat.ast.ASTNode> parse(String c, Category start) {
+    // System.out.println("================================================================================");
+    // System.out.println("Parsing: \"" + c + "\"");
+    // System.out.println("================================================================================");
 
-		java.util.List<Symbol> tokens = CTestUtil.scan(new lang.c.pat.ast.PatLangScanner(new StringReader(c)));
+    java.util.List<Symbol> tokens = CTestUtil.scan(new lang.c.pat.ast.PatLangScanner(new StringReader(c)));
 
-		var astBuilder = lang.c.pat.ast.ASTBuilder.getInstance();
-		java.util.List<ParseTree> parseTrees = CTestUtil.parse(tokens, start,
-															   astBuilder,
-															   lang.c.pat.ast.PatLangParserSEP.Terminals.NAMES);
+    var astBuilder = lang.c.pat.ast.ASTBuilder.getInstance();
+    java.util.List<ParseTree> parseTrees = CTestUtil.parse(tokens, start,
+                                                           astBuilder,
+                                                           lang.c.pat.ast.PatLangParserSEP.Terminals.NAMES);
 
-		assertNotNull(parseTrees);
+    assertNotNull(parseTrees);
 
-		return CTestUtil.buildAST(tokens, parseTrees, astBuilder);
-	}
+    return CTestUtil.buildAST(tokens, parseTrees, astBuilder);
+  }
 
-	@Test public void test1() {
-		var roots = parse("for(;;)  ;", PatLangParserSEP.n_for_statement);
+  @Test public void test1() {
+    var roots = parse("for(;;)  ;", PatLangParserSEP.n_for_statement);
 
-		for (var root : roots) {
-			System.out.println(root.matcher().genMatcher());
-		}
-	}
-
-  @Disabled
-	@Test public void test2() {
-		var roots = parse("for(;;) $body ", PatLangParserSEP.n_for_statement);
-
-		for (var root : roots) {
-			System.out.println(root.matcher().genMatcher());
-		}
-	}
+    for (var root : roots) {
+      System.out.println(root.matcher().genMatcher());
+    }
+  }
 
   @Disabled
-	@Test public void test3() {
-		var roots = parse("for($init; $cond; $inc) $body ", PatLangParserSEP.n_for_statement);
+  @Test public void test2() {
+    var roots = parse("for(;;) $body ", PatLangParserSEP.n_for_statement);
 
-		for (var root : roots) {
-			System.out.println(root.matcher().genMatcher());
-		}
-	}
+    for (var root : roots) {
+      System.out.println(root.matcher().genMatcher());
+    }
+  }
 
   @Disabled
-	@Test public void test4() {
-		var roots = parse("for($init $cond; $inc) $body ", PatLangParserSEP.n_for_decl_statement);
+  @Test public void test3() {
+    var roots = parse("for($init; $cond; $inc) $body ", PatLangParserSEP.n_for_statement);
 
-		for (var root : roots) {
-			System.out.println(root.matcher().genMatcher());
-		}
-	}
+    for (var root : roots) {
+      System.out.println(root.matcher().genMatcher());
+    }
+  }
+
+  @Disabled
+  @Test public void test4() {
+    var roots = parse("for($init $cond; $inc) $body ", PatLangParserSEP.n_for_decl_statement);
+
+    for (var root : roots) {
+      System.out.println(root.matcher().genMatcher());
+    }
+  }
 
   static List<String> genMatchers(String s, Category c) {
     List<String> matchers = new ArrayList<>();

d68b8c27f225fa36dbb919ae9f7ca2319a57ac79
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-06-14 10:42:39 +0200

    Uninitialized variable - avoid false positives

--- tests/clang/evaluation/src/uninitialized_variable/CWE457.c
+++ tests/clang/evaluation/src/uninitialized_variable/CWE457.c
@@ -48,3 +48,16 @@ void CWE457_Use_of_Uninitialized_Variable__double_array_alloca_no_init_16_bad()
         break;
     }
 }
+
+void printIntLine(int);
+static void goodG2B()
+{
+    int * data;
+    /* FIX: Initialize data */
+    /* initialize both the pointer and the data pointed to */
+    data = (int *)malloc(sizeof(int));
+    if (!data) {exit(-1);}
+    *data = 5;
+    /* POTENTIAL FLAW: Use data without initializing it */
+    printIntLine(*data);
+}

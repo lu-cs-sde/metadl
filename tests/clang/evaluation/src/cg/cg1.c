d47c2ac0f3d84036898d96e812adb606bee955a4
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-08-31 14:23:53 +0200

    Some input file for testing call graph computation

---
+++ tests/clang/evaluation/src/cg/cg1.c
@@ -0,0 +1,16 @@
+int foo(int x, int y) {
+  return x + y;
+}
+
+/* int bar () { */
+/*   int a, b; */
+/*   a = 3; */
+/*   b = 5; */
+
+/*   int c; */
+/*   c = foo(a, b); */
+
+/*   int d = foo(3, a); */
+
+/*   return c + d; */
+/* } */

27dae797a5842ec5bf5a14a5de6ac5414e7e4a51
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-11-07 22:03:35 +0100

    Update the Docker build

--- docker/build.sh
+++ docker/build.sh
@@ -6,11 +6,11 @@ docker build \
        --build-arg JAVADL_GIT=https://github.com/lu-cs-sde/metadl.git \
        --build-arg JAVADL_HASH=751f63917b980e3ec652d83fb5d7de9995773540 \
        --build-arg CLOG_EVAL_GIT=https://github.com/alexdura/clog-eval.git \
-       --build-arg CLOG_EVAL_HASH=79d86f6c6e58756980478790eb2242a3472f839e \
+       --build-arg CLOG_EVAL_HASH=1cedb92199bc6d2539b635efa6f8998a9915fe6c \
        --build-arg LLVM_GIT=https://github.com/alexdura/llvm-project.git \
        --build-arg LLVM_HASH=c9a1d5ea1edc7439f2213fd42a080828d0e7de22 \
        --build-arg JULIET_GIT=https://github.com/alexdura/juliet.git \
        --build-arg JULIET_HASH=dc4849eb6072f1fb1dec861aa2eb0e34ba379c1a \
        --build-arg MAGMA_GIT=https://github.com/alexdura/magma.git \
-       --build-arg MAGMA_HASH=2b5dfb428ad9d8fef3c9e6c3d02a7695649b2c35 \
+       --build-arg MAGMA_HASH=f0f88cb22bec4054e0a1241d77f300d70106e5b1 \
        -t clog23 .

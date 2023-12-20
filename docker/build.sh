b87a485b1d23116574d9c8c6218623cb49465df0
Alexandru Dura <alexandru.dura@cs.lth.se>
2023-12-20 16:24:41 +0100

    Update the Clog eval version

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ docker build \
        --build-arg JAVADL_GIT=https://github.com/lu-cs-sde/metadl.git \
        --build-arg JAVADL_HASH=8f119b7b2a6a23eeb1a48e9e168d22d2b26849e7 \
        --build-arg CLOG_EVAL_GIT=https://github.com/alexdura/clog-eval.git \
-       --build-arg CLOG_EVAL_HASH=3f62b40a24f5850841768bfdb0a310ade20054a9 \
+       --build-arg CLOG_EVAL_HASH=a4cedd636513e9dbe37ef2c02b317c2bc60d2d91 \
        --build-arg LLVM_GIT=https://github.com/alexdura/llvm-project.git \
        --build-arg LLVM_HASH=c9a1d5ea1edc7439f2213fd42a080828d0e7de22 \
        --build-arg JULIET_GIT=https://github.com/alexdura/juliet.git \
